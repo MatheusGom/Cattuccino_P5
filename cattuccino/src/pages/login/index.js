b0b6baf4ebb2c7c4e22664e10f4d73dfb63e6122
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2024-10-17 18:00:43 -0300

    refactor: change initial background to component

--- cattuccino/src/pages/login/index.js
+++ cattuccino/src/pages/login/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import styles from './Login.module.css';
+import InitialBackground from '../../components/InitialBackground';
 
 function Login() {
     return (
         <>
-        <div className={styles.title}>
-            <h1>Bem Vindo!</h1>
-            <p>Seja bem vindo ao nosso site de análises do Cattuccino</p>
-        </div>
-        <div className={styles.container}>
-            <form className={styles['login-form']}>
-                <img className={styles.logo} src='/images/logo.svg' alt='Logo' /><br />
-                <p className={styles.subtitle}>Por favor, nos diga como você deseja entrar</p>
-                <div className={styles.options}>
-                    <div className={styles['selection-box']}>
-                        <img className={styles['box-image']} src='/images/user.svg' alt='User' />
-                        Usuário
+            <InitialBackground/>
+            <div className={styles.container}>
+                <form className={styles['login-form']}>
+                    <img className={styles.logo} src='/images/logo.svg' alt='Logo' /><br />
+                    <p className={styles.subtitle}>Por favor, nos diga como você deseja entrar</p>
+                    <div className={styles.options}>
+                        <div className={styles['selection-box']}>
+                            <img className={styles['box-image']} src='/images/user.svg' alt='User' />
+                            Usuário
+                        </div>
+                        <div className={styles['selection-box']}>
+                            <img className={styles['box-image']} src='/images/manager.svg' alt='Manager' />
+                            Gerente
+                        </div>
                     </div>
-                    <div className={styles['selection-box']}>
-                        <img className={styles['box-image']} src='/images/manager.svg' alt='Manager' />
-                        Gerente
-                    </div>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
         </>
     );
 }

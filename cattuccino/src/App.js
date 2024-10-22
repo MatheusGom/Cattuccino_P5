8257df86561c09f2677cf447695aa3c1c932ff35
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2024-10-22 18:41:39 -0300

    add: begin component structure

--- cattuccino/src/App.js
+++ cattuccino/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./pages/login";
 import UserLogin from './pages/userLogin';
+import TestPage from './pages/testPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Login/>} />
-        <Route path='/usuario/:userType' element={<UserLogin/>}/>
+        <Route path='/' element={<Login />} />
+        <Route path='/usuario/:userType' element={<UserLogin />} />
+        <Route path='/test' element={<TestPage />}/>
       </Routes>
     </Router>
   );

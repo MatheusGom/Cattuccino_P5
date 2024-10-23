7cfbfde3deefa2ecfec10846a74709f6402ad3da
Diogo Henrique <116087739+DiogoHMC@users.noreply.github.com>
2024-10-23 16:41:09 -0300

    :sparkles: add: Starting sidebar

---
+++ cattuccino/src/components/Sidebar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import styles from './InitialBackground.module.css' TIRA ESSE COMENTARIO NA HORA DE USAR O CSS
+
+function Sidebar() {
+    return (
+        <>
+            <h1>AQUI É O SIDE BAR</h1>
+        </>
+    );
+}
+
+export default Sidebar;

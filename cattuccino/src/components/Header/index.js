7b6006a7894aa082183f39943e5061886cbc6d1a
paulo <paulo.m.campos6601@gmail.com>
2024-10-24 07:14:27 -0300

    style: add hover animation

--- cattuccino/src/components/Header/index.js
+++ cattuccino/src/components/Header/index.js
@@ -6,20 +6,20 @@ function Header() {
         <>
             <div className={styles.head}>
                 <h3 className={styles['head-title']}>Dashboard</h3>
-                <div className={styles.info}>
-                <img className={styles['search-icon']} src='/images/Icon.svg' alt='search' />
-                <input
-                    className={styles['form-input']}
-                    type='search'
-                    placeholder='search'
-                            />
-                </div>
                 <div className={styles['selection-box']}>
-                <img className={styles['box-image']} src='/images/Notificação.svg' alt='Notificação' />
-                <img className={styles['box-image']} src='/images/Noturno.svg' alt='Modo Noturno' />                    
-                <img className={styles['box-image']} src='/images/Configuração.svg' alt='Configuração' />                    
-                <img className={styles['box-image']} src='/images/Perfil Nav Bar.svg' alt='Perfil Nav Bar' />                    
-            </div>
+                    <div className={styles.info}>
+                        <img className={styles['search-icon']} src='/images/Icon.svg' alt='search' />
+                        <input
+                            className={styles['form-input']}
+                            type='search'
+                            placeholder='Buscar...'
+                        />
+                    </div>
+                    <img className={styles['box-image']} src='/images/Notificação.svg' alt='Notificação' />
+                    <img className={styles['box-image']} src='/images/Noturno.svg' alt='Modo Noturno' />
+                    <img className={styles['box-image']} src='/images/Configuração.svg' alt='Configuração' />
+                    <img className={styles['box-image']} src='/images/Perfil Nav Bar.svg' alt='Perfil Nav Bar' />
+                </div>
             </div>
         </>
     );

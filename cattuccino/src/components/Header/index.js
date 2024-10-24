e5f9cff17cc3a2fc28f9f65ecf2cebb5d1d3fe27
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2024-10-24 14:27:26 -0300

    fix: adapt search-icon animation

--- cattuccino/src/components/Header/index.js
+++ cattuccino/src/components/Header/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { useState } from 'react';
 
 function Header() {
+
+    const [searchValue, setSearchValue] = useState('');
+
     return (
         <>
             <div className={styles.head}>
                 <h3 className={styles['head-title']}>Dashboard</h3>
                 <div className={styles['container']}>
                     <div className={styles.info}>
-                        <img className={styles['search-icon']} src='/images/Icon.svg' alt='search' />
+                        <img
+                            className={`${styles['search-icon']} ${searchValue ? styles['icon-active'] : ''}`}
+                            src='/images/Icon.svg'
+                            alt='search'
+                        />
                         <input
                             className={styles['form-input']}
                             type='search'
                             placeholder='Buscar...'
+                            value={searchValue}
+                            onChange={(e) => setSearchValue(e.target.value)}
                         />
                     </div>
                     <img className={styles['box-image']} src='/images/Notificação.svg' alt='Notificação' />

830d6d5116c5a734424d47c2fd1b703813e4bc48
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2024-10-25 08:47:37 -0300

    fix: remove test pages

--- cattuccino/src/App.js
+++ cattuccino/src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from "./pages/login";
 import UserLogin from './pages/userLogin';
-import TestPage from './pages/testPage';
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/usuario/:userType' element={<UserLogin />} />
-        <Route path='/test' element={<TestPage />}/>
       </Routes>
     </Router>
   );

0744c29703ca36e23396c0329f2680f8609da280
paixaoao <aptm@cesar.school>
2024-11-05 11:59:20 -0300

    style:developing dashboard marketing (left side)

--- cattuccino/src/pages/dashboard/index.js
+++ cattuccino/src/pages/dashboard/index.js
@@ -18,29 +18,82 @@ function Dashboard() {
                         <h1>Marketing</h1>
                 
                         <div className={styles.faturamentoDiario}>
-                            <div>
-                                <img src="/images/Frame 80.png" alt="Descrição da Imagem" />
+                            <div className={styles.infoClientes}>
+                                <p className={styles.titulo}>Novos Clientes</p>
+                                <div className={styles.dados}>
+                                    <span className={styles.numero}>+3.052</span>
+                                    <span className={styles.percentual}>+14%</span>
+                                </div>
                             </div>
                             <div className={styles.icone}>
-                            <img
-                            src='/images/adicionar_pessoa.svg'
-                            alt='add'
-                        />
-                        </div>
+                                <img
+                                    src='/images/adicionar_pessoa.svg'
+                                    alt='add'
+                                />
+                            </div>
                         </div>
 
                         <div className={styles.caixaRedesSociais}>
-                            <h1>Usuários ativos</h1>
-                            <h2>Distribuição das redes sociais</h2>
-                            {/* Aqui você pode adicionar o conteúdo específico para essa caixa */}
+                        <div className={styles.infoClientes}>
+                                <p className={styles.tituloRedesSociais}>Usuários ativos</p>
+                                <p className={styles.subtituloRedesSociais}>Distribuição das redes sociais</p>
+                                
+                            </div>
+
+                            <div className={styles.iconepizza}>
+                                <img
+                                    src='/images/GraficoPizza.svg'
+                                    alt='grafico pizza'
+                                />
+                            </div>
+                            <div className={styles.iconeredes}>
+                                <img
+                                    src='/images/redessociais.svg'
+                                    alt='redessociais'
+                                />
+                            </div>
                         </div>
 
-                        {/* Nova caixa para usuários ativos geral */}
-                        <div className={styles.caixaUsuariosAtivos}>
-                            <h2>Usuários Ativos Geral</h2>
-                            {/* Aqui você pode adicionar o conteúdo específico para essa caixa */}
+                    <div className={styles.caixaUsuariosAtivos}>
+                            <div className={styles.graficoatividade}>
+                            <img
+                                    src='/images/GraficoAtivos.svg'
+                                    alt='Grafico Ativos'
+                                />
+
+                             </div>   
+                             <p className={styles.tituloRedesSociais}>Usuários ativos</p>
+                             <div className={styles.linhaPercentual}>
+                            <span className={styles.percentual}>(+23%)</span>
+                            <p className={styles.subtituloRedesSociais}>que na última semana</p>
                         </div>
+                        <p className={styles.tituloRedesSociais}>Dados dos Usuários</p>
+
+                        <div className={styles.container}>
+                        <img className={styles['box-image']} src='/images/Usuariosredes.svg' alt='Usuarios da rede' />
+                        <img className={styles['box-image']} src='/images/Clickredes.svg' alt='cliques' />
+                        <img className={styles['box-image']} src='/images/Vendasredes.svg' alt='Vendas das redes' />
+                        <img className={styles['box-image']} src='/images/ItensRedes.svg' alt='Itens' />
+                                        
+                        </div>
+                        <div className={styles.containernumeros}>
+                        <p className={styles.numeroIndividual}>32,904</p>
+                        <p className={styles.numeroIndividual}>2,42m</p>
+                        <p className={styles.numeroIndividual}>2,400$</p>
+                        <p className={styles.numeroIndividual}>320</p>
+                        </div>
+                        <div className={styles.containerlinha}>
+                        <img className={styles['box-image2']} src='/images/linhadados.svg' alt='linha' />
+                        <img className={styles['box-image2']} src='/images/linhadados.svg' alt='linha' />
+                        <img className={styles['box-image2']} src='/images/linhadados.svg' alt='linha' />
+                        <img className={styles['box-image2']} src='/images/linhadados.svg' alt='linha' />
+                                        
+                        </div>
+
+
+
                     </div>
+            </div>
                 ) : (
                     <div className={styles.generalDashboard}>
                         {/* Conteúdo do dashboard geral */}

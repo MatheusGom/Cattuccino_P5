3dfcf40ba072c831d73f913b696b59b2f17eaafd
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2024-11-21 15:16:16 -0300

    style: finishing users screen style

--- frontend/src/pages/userPage/index.js
+++ frontend/src/pages/userPage/index.js
@@ -71,13 +71,16 @@ const UsuariosPage = () => {
 
     return (
         <>
+            <Header activeButton={activeButton} />
             <div className={styles['users-background']}>
-                <Header activeButton={activeButton} />
                 <Sidebar userType={userType} activeButton={activeButton} setActiveButton={setActiveButton} />
                 <div className={styles['users-body']}>
                     <div className={styles['users-list']}>
-                        <div className={styles['add-member-btn']} onClick={() => navigate('/add-member')}>
-                            +
+                        <div className={styles['add-container']}>
+                            <h2>Adicionar membro</h2>
+                            <div className={styles['add-member-btn']} onClick={() => navigate('/dashboard/2/usuarios/novo')}>
+                                <img src='/images/addBtn.svg' alt="Botão para adicionar novo funcionário"/>
+                            </div>
                         </div>
                         {usuarios.map(usuario => (
                             <div
@@ -94,20 +97,39 @@ const UsuariosPage = () => {
                         {selectedUser ? (
                             <div className={styles.info}>
                                 <h2>{selectedUser.NOME}</h2>
-                                <p><strong>Idade:</strong> {selectedUser.IDADE || 'N/A'}</p>
-                                <p><strong>Localização:</strong> {selectedUser.LOCALIZACAO || 'N/A'}</p>
-                                <p><strong>Tipo Sanguíneo:</strong> {selectedUser.TIPO_SANGUINEO || 'N/A'}</p>
-                                <p><strong>Formação:</strong> {selectedUser.FORMACAO || 'N/A'}</p>
-                                <p><strong>Data de Nascimento:</strong> {selectedUser.DT_NASCIMENTO || 'N/A'}</p>
-                                <p><strong>Cor:</strong> {selectedUser.COR || 'N/A'}</p>
-                                <p><strong>Gênero:</strong> {selectedUser.GENERO || 'N/A'}</p>
-                                <p><strong>Data de Admissão:</strong> {selectedUser.DT_ADMISSAO || 'N/A'}</p>
-                                <div className={styles['user-ratings']}>
-                                    <p><strong>{getAttributeLabel('Extrovertido', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.EXTROVERTID)}</p>
-                                    <p><strong>{getAttributeLabel('Criativo', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.CRIATIV)}</p>
-                                    <p><strong>{getAttributeLabel('Ocupado', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.OCUPAD)}</p>
-                                    <p><strong>{getAttributeLabel('Organizado', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.ORGANIZAD)}</p>
-                                    <p><strong>{getAttributeLabel('Tranquilo', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.TRANQUIL)}</p>
+                                <p className={styles['card-subtitle']}>{selectedUser.GERENCIA === 1 ? 'Gerente' : 'Funcionário'}</p>
+                                <div className={styles['card-content']}>
+                                    <div>
+                                        <p><strong>Idade:</strong> {selectedUser.IDADE || 'N/A'}</p>
+                                        <p><strong>Localização:</strong> {selectedUser.LOCALIZACAO || 'N/A'}</p>
+                                        <p><strong>Tipo Sanguíneo:</strong> {selectedUser.TIPO_SANGUINEO || 'N/A'}</p>
+                                        <p><strong>Formação:</strong> {selectedUser.FORMACAO || 'N/A'}</p>
+                                        <p><strong>Data de Nascimento:</strong> {selectedUser.DT_NASCIMENTO || 'N/A'}</p>
+                                        <p><strong>Cor:</strong> {selectedUser.COR || 'N/A'}</p>
+                                        <p><strong>Gênero:</strong> {selectedUser.GENERO || 'N/A'}</p>
+                                        <p><strong>Data de Admissão:</strong> {selectedUser.DT_ADMISSAO || 'N/A'}</p>
+                                    </div>
+                                    <div className={styles['user-ratings']}>
+                                        <p><strong>{getAttributeLabel('Extrovertido', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.EXTROVERTID)}</p>
+                                        <p><strong>{getAttributeLabel('Criativo', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.CRIATIV)}</p>
+                                        <p><strong>{getAttributeLabel('Ocupado', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.OCUPAD)}</p>
+                                        <p><strong>{getAttributeLabel('Organizado', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.ORGANIZAD)}</p>
+                                        <p><strong>{getAttributeLabel('Tranquilo', selectedUser.GENERO)}:</strong> {renderPaws(selectedUser.TRANQUIL)}</p>
+                                    </div>
+                                </div>
+                                <div className={styles['button-container']}>
+                                    <button className={styles['card-button']}>
+                                        Excluir Membro
+                                        <div className={styles['image-btn']}>
+                                            <img src='/images/trashCan.svg' alt='Botão para excluir usuário'/>
+                                        </div>
+                                    </button>
+                                    <button className={styles['card-button']}>
+                                        Editar Membro
+                                        <div className={styles['image-btn']}>
+                                            <img src='/images/pencil.svg' alt='Botão para editar usuário'/>
+                                        </div>
+                                    </button>
                                 </div>
                             </div>
                         ) : (

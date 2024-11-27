077a6766c30df410b66db49eb640678a0aba0ae2
unknown <mga@cesar.school>
2024-11-27 15:34:25 -0300

    fix: Implementing login routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -6,6 +6,21 @@ from sqlalchemy.sql import func
 
 usuarios_bp = Blueprint('usuarios_bp', __name__)
 
+@usuarios_bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('EMAIL')
+    senha = data.get('SENHA')
+
+    if not email or not senha:
+        return jsonify({'message': 'Email e senha são obrigatórios'}), 400
+
+    usuario = Usuario.query.filter_by(EMAIL=email, SENHA=senha).first()
+    
+    if usuario:
+        return jsonify({'message': 'Login realizado com sucesso!', 'user_id': usuario.ID}), 200
+    else:
+        return jsonify({'message': 'Credenciais inválidas, tente novamente'}), 401
 
 @usuarios_bp.route('/usuarios', methods=['POST'])
 def create_usuario():
